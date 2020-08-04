03bba559dfc873788a43479d572b3c3ae92ca31d
Kevin Lee <keverino@Kevins-MBP.home>
2020-08-03 18:55:28 -0700

    add function to wait for user input.

--- tweets.js
+++ tweets.js
@@ -1,23 +1,26 @@
 const brain = require("brain.js");
 const data = require("./twitter.json");
+const prompt = require("prompt");
 
 const network = new brain.recurrent.LSTM();
 
-const trainingData = data.map(item => ({
+const trainingData = data.map((item) => ({
   input: item.text,
-  output: item.category
+  output: item.category,
 }));
 
-train = data => {
+prompt.start();
+
+train = (data) => {
   console.log("\n - Training started\n");
   network.train(data, {
     iterations: 100,
-    log: true
+    log: true,
   });
   console.log("\n - Training complete\n");
 };
 
-execute = input => {
+execute = (input) => {
   console.log("----------------------------------");
   console.log(`Running against input:`);
   console.log("----------------------------------");
@@ -32,12 +35,37 @@ execute = input => {
 };
 
 // checks if the trainingData has the input
-checkForInput = input => {
-  console.log(trainingData.some(item => item.text === input));
+checkForInput = (input) => {
+  console.log(trainingData.some((item) => item.text === input));
 };
 
+// train
 train(trainingData);
 
+function waitForUserInput() {
+  console.log("\n----------------------------------");
+  console.log(`Enter a sentence to run against training data.`);
+  console.log("If you're done and want to exit, enter 'exit'.");
+  console.log("----------------------------------");
+
+  prompt.get(["input"], function (err, result) {
+    if (err) {
+      console.log(err);
+      return 1;
+    }
+
+    if (result.input !== "exit") {
+      console.log("Command-line input received. \n\n");
+      execute(result.input);
+      waitForUserInput();
+    }
+  });
+}
+
+waitForUserInput();
+
+/*
 execute(
   "We are in very close communication with China concerning the virus. Very few cases reported in USA, but strongly on watch. We have offered China and President Xi any help that is necessary. Our experts are extraordinary!"
 );
+*/
