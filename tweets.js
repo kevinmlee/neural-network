6b9aace4d3ba39a93c48b08e910cee3a583bbf97
Kevin Lee <kmlee1127@gmail.com>
2022-04-17 17:22:36 -0700

    add ability to save and load from trained data

--- tweets.js
+++ tweets.js
@@ -1,9 +1,11 @@
 const brain = require("brain.js");
 const data = require("./twitter.json");
 const prompt = require("prompt");
+const fs = require("fs");
 
 const network = new brain.recurrent.LSTM();
 
+// load the training sample data
 const trainingData = data.map((item) => ({
   input: item.text,
   output: item.category,
@@ -18,6 +20,9 @@ train = (data) => {
     log: true,
   });
   console.log("\n - Training complete\n");
+
+  // save the trained network
+  fs.writeFileSync("trained.json", JSON.stringify(network.toJSON()));
 };
 
 execute = (input) => {
@@ -26,7 +31,7 @@ execute = (input) => {
   console.log("----------------------------------");
   console.log(`"${input}"`);
 
-  let results = network.run(input);
+  const results = network.run(input);
 
   console.log("\n----------------------------------");
   console.log(`Best guess:`);
@@ -39,8 +44,18 @@ checkForInput = (input) => {
   console.log(trainingData.some((item) => item.text === input));
 };
 
-// train
-train(trainingData);
+// check if network has already been trained
+// if not, train it
+console.log("Looking for trained network...");
+if (fs.existsSync("trained.json")) {
+  console.log("Reading trained network from file");
+  const trainedData = JSON.parse(fs.readFileSync("trained.json"));
+  network.fromJSON(trainedData);
+} else {
+  console.log("No trained network found. Training...");
+  train(trainingData);
+  console.log("Neural network ready.");
+}
 
 function waitForUserInput() {
   console.log("\n----------------------------------");
@@ -55,7 +70,7 @@ function waitForUserInput() {
     }
 
     if (result.input !== "exit") {
-      console.log("Command-line input received. \n\n");
+      console.log("\nCommand-line input received. \n\n");
       execute(result.input);
       waitForUserInput();
     }
